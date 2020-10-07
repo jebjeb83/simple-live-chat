7ac55066e2499776e5be4c3c272979ff756fb051
Alfie Arcede <jebjeb83@gmail.com>
2020-10-07 13:38:13 +0800

    Updates

---
+++ precache-manifest.896f4de7e551d492293c4e67be4f48b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12dca9fa86771b807b8773f7ef6a05ca",
+    "url": "/simple-live-chat/index.html"
+  },
+  {
+    "revision": "9897453a5a055f4572a4",
+    "url": "/simple-live-chat/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "ba5d359a21d97cdb9608",
+    "url": "/simple-live-chat/static/css/main.1f3effd8.chunk.css"
+  },
+  {
+    "revision": "9897453a5a055f4572a4",
+    "url": "/simple-live-chat/static/js/2.b232f60b.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/simple-live-chat/static/js/2.b232f60b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba5d359a21d97cdb9608",
+    "url": "/simple-live-chat/static/js/main.62d5e6aa.chunk.js"
+  },
+  {
+    "revision": "e58b1ed88c9bef7d3445",
+    "url": "/simple-live-chat/static/js/runtime-main.53c0f1d6.js"
+  }
+]);
